64049e9c9cd4fb3b8e0096c7945fd92211ab408e
marcinbalut <marcinbalutpoland@gmail.com>
2022-12-12 12:25:42 +0100

    Added UserLike as entity and join table for like feature

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -23,5 +23,8 @@ namespace API.Entities
         public string City { get; set; }
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
+
+        public List<UserLike> LikedByUsers { get; set; }
+        public List<UserLike> LikedUsers { get; set; }
     }
 }
