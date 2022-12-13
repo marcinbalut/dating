30a0c9e471aaf2907a5646db3997e39ad22a727d
marcinbalut <marcinbalutpoland@gmail.com>
2022-12-13 16:18:28 +0100

    Added Message relationship with AppUser and created new db migration

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -15,6 +15,7 @@ namespace API.Data
 
         public DbSet<AppUser> Users { get; set; }
         public DbSet<UserLike> Likes { get; set; }
+        public DbSet<Message> Messages { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -33,7 +34,18 @@ namespace API.Data
                 .HasOne(t => t.TargetUser)
                 .WithMany(l => l.LikedByUsers)
                 .HasForeignKey(t => t.TargetUserId)
-                .OnDelete(DeleteBehavior.Cascade);            
+                .OnDelete(DeleteBehavior.Cascade);
+
+
+            builder.Entity<Message>()
+                .HasOne(u => u.Recipient)
+                .WithMany(m => m.MessagesReceived)
+                .OnDelete(DeleteBehavior.Restrict);
+            
+            builder.Entity<Message>()
+                .HasOne(u => u.Sender)
+                .WithMany(m => m.MessagesSent)
+                .OnDelete(DeleteBehavior.Restrict);
         }        
     }
 }
