099a66cb93be3cf1d1eab992b7ecc67f66d182d7
marcinbalut <marcinbalutpoland@gmail.com>
2022-12-16 16:31:59 +0100

    Updated seeding mechanism

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -6,25 +6,26 @@ using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
     public class Seed
     {
-        public static async Task SeedUsers(DataContext context)
+        public static async Task SeedUsers(UserManager<AppUser> userManager)
         {
-            if (await context.Users.AnyAsync()) return;
+            if (await userManager.Users.AnyAsync()) return;
 
             var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
+
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
+
             foreach (var user in users)
             {
                 user.UserName = user.UserName.ToLower();
-                context.Users.Add(user);
+                await userManager.CreateAsync(user, "!234Pass");
             }
-
-            await context.SaveChangesAsync();
         }
     }
 }
