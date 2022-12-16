fc095c9544e284fbb90ebb83a67601391c623e1c
marcinbalut <marcinbalutpoland@gmail.com>
2022-12-16 15:13:14 +0100

    Added DI services configuration for ASP Identity

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 
 namespace API.Extensions
@@ -11,8 +14,16 @@ namespace API.Extensions
     public static class IdentityServiceExtensions
     {
         public static IServiceCollection AddIdentityServices(
-            this IServiceCollection services, IConfiguration config)
-        {
+            this IServiceCollection services, IConfiguration config)        {
+
+            services.AddIdentityCore<AppUser>(opt => 
+                {
+                    opt.Password.RequireNonAlphanumeric = false;
+                })
+            .AddRoles<AppRole>()
+            .AddRoleManager<RoleManager<AppRole>>()
+            .AddEntityFrameworkStores<DataContext>();
+
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(
                     options => options.TokenValidationParameters = new TokenValidationParameters
