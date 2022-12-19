860dea5993074b8edbebc7a8a0a35779504a57ec
marcinbalut <marcinbalutpoland@gmail.com>
2022-12-19 16:50:58 +0100

    Added authorization roles policy

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -33,6 +33,12 @@ namespace API.Extensions
                         ValidateIssuer = false,
                         ValidateAudience = false
                     });
+            
+            services.AddAuthorization(opt => 
+            {
+                opt.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+                opt.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
+            });
 
             return services;
         }
