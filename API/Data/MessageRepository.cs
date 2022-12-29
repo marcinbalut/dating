026a2d2babc84f525840f8f2a49fa29aed542591
marcinbalut <marcinbalutpoland@gmail.com>
2022-12-29 14:12:09 +0100

    Updated chosen queries in terms of efficiency

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -82,9 +82,7 @@ namespace API.Data
 
         public async Task<IEnumerable<MessageDto>> GetMessageThread(string currentUserName, string recipientUserName)
         {
-            var messages = await _context.Messages
-                .Include(u => u.Sender).ThenInclude(p => p.Photos)
-                .Include(u => u.Recipient).ThenInclude(p => p.Photos)
+            var query = _context.Messages
                 .Where(
                     m => m.RecipientUsername == currentUserName && m.RecipientDeleted == false && 
                     m.SenderUsername == recipientUserName ||
@@ -92,9 +90,9 @@ namespace API.Data
                     m.SenderUsername == currentUserName
                 )
                 .OrderBy(m => m.MessageSent)
-                .ToListAsync();
+                .AsQueryable();
             
-            var unreadMessages = messages.Where(m => m.DateRead == null
+            var unreadMessages = query.Where(m => m.DateRead == null
                 && m.RecipientUsername == currentUserName).ToList();
             
             if (unreadMessages.Any())
@@ -105,7 +103,7 @@ namespace API.Data
                 }
             }
 
-            return _mapper.Map<IEnumerable<MessageDto>>(messages);
+            return await query.ProjectTo<MessageDto>(_mapper.ConfigurationProvider).ToListAsync();
         }
 
         public void RemoveConnection(Connection connection)
