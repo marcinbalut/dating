98a73f8934c957323935eb0e1aec9a16cc480cc2
marcinbalut <marcinbalutpoland@gmail.com>
2022-12-30 15:11:06 +0100

    Deployment: Deleted fake delay from production mode

--- client/src/app/_interceptors/loading.interceptor.ts
+++ client/src/app/_interceptors/loading.interceptor.ts
@@ -5,9 +5,10 @@ import {
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { identity, Observable } from 'rxjs';
 import { BusyService } from '../_services/busy.service';
 import { delay, finalize } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class LoadingInterceptor implements HttpInterceptor {
@@ -18,7 +19,7 @@ export class LoadingInterceptor implements HttpInterceptor {
     this.busyService.busy();
 
     return next.handle(request).pipe(
-      delay(300),
+      (environment.production ? identity : delay(500)),
       finalize(() => {
         this.busyService.idle();
       })
